578c402ea6c4d1a6024f0de53b5d3afea120367d
lovemooner <lovemooner@yeah.net>
2019-01-16 15:20:50 +0800

    up

--- src/main/java/love/moon/filter/SecurityFilter.java
+++ src/main/java/love/moon/filter/SecurityFilter.java
@@ -3,7 +3,6 @@ package love.moon.filter;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class SecurityFilter implements Filter {
