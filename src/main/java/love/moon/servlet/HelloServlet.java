c00f02c4e204a4f8e330b5e2c6ba2928d2b67c50
lovemooner <lovemooner@yeah.net>
2019-03-14 01:00:21 +0800

    nio

--- src/main/java/love/moon/servlet/HelloServlet.java
+++ src/main/java/love/moon/servlet/HelloServlet.java
@@ -12,24 +12,25 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request,
                          HttpServletResponse response) throws IOException {
         System.out.println("coming request");
-        String output="Hello Servlet";
-        //Session Test
-        if(request.getSession().getAttribute("count") == null){
-            request.getSession().setAttribute("count", 0);
-            response.getWriter().write(output+0);
-        }else{
-            int a = Integer.parseInt(request.getSession().getAttribute("count").toString());
-            request.getSession().setAttribute(output+",count", ++a);
-            response.getWriter().write(a+"");
+        BufferedReader br = request.getReader();
+        String str, wholeStr = "";
+        while ((str = br.readLine()) != null) {
+            wholeStr += str;
         }
+        System.out.println(wholeStr);
 
+        String output = "Hello Servlet";
         try {
-            System.out.println("Thread "+Thread.currentThread()+" will sleep");
-            Thread.sleep(1000*20l);
+            System.out.println("Thread " + Thread.currentThread() + " will sleep");
+            Thread.sleep(1000 * 2l);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         response.getWriter().println(output);
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req,resp);
     }
 }
